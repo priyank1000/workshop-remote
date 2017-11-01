7e8c001d7be7dbb9c5665880288335f6790ba656
priyank1000 <priyank10@hotmail.com>
2017-11-01 18:43:35 +0800

    Add files via upload

--- fuelwatch.py
+++ fuelwatch.py
@@ -1,65 +1,47 @@
-link1 = 'http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=2&Suburb=Mandurah'
-link2 = 'http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=4&Suburb=Mindarie&Day=tomorrow'
-import urllib.request
-import urllib.parse
-import io
+north = 'http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=2&Region=25'
+south = 'http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=2&Region=26'
+northTomorrow = 'http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=2&Region=26%Day=tomorrow'
+southTomorrow = 'http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=2&Region=25&Day=tomorrow'
+
 import time
 from lxml import etree
 from operator import itemgetter
 from urllib.request import urlopen
 
 dictionary = {}
-list = []
-listTomorrow = []
-links = [link1]
+arrayList = [north, south, northTomorrow, southTomorrow]
+array = []
 tableContent = ""
 htmlContent = ""
 colour = ""
 
-for link in links:
-	response = urlopen(link)
+for list in arrayList:
+	response = urlopen(list)
 	value = response.read()
-	content = etree.iterparse(io.BytesIO(value))
+	content = etree.fromstring(value)
+	items = content.findall('.//item')
 	
-	i = 0
-	for action, element in content:
-		if i == 5:
-			list.append(dictionary)
-			dictionary = {}
-			i = 0
-			
-		if element.text != None:
-			#print(element.tag + "-" + element.text)
-				
-			if element.tag == "location":
-				#location.append(element.text)
-				dictionary[element.tag] = element.text
-				i = i + 1
-				
-			elif element.tag == "product":
-				dictionary[element.tag] = element.text
-				i = i + 1
-				
-			elif element.tag == "price":
-				dictionary[element.tag] = element.text
-				i = i + 1
-				
-			elif element.tag == "address":
-				dictionary[element.tag] = element.text
-				i = i + 1
-				
-			elif element.tag == "brand":
-				dictionary[element.tag] = element.text
-				i = i + 1
-				
+	for item in items:
+		#dictionary['title'] = item.find('./title').text 
+		dictionary['price'] = item.find('./price').text
+		#dictionary['description'] = item.find('./description').text
+		dictionary['address'] = item.find('./address').text
+		dictionary['location'] = item.find('./location').text
+		dictionary['brand'] = item.find('./brand').text
+		
+		array.append(dictionary)
+		dictionary = dict()
+		
+		
+	
 #the list has a number of dictionary
 # list can be sorted but not dictionary (you can sort a list of dictionary)
 #sort by price
-sortedList = sorted(list, key = itemgetter('price'))
+sortedList = sorted(array, key = itemgetter('price'))
 
 colourContent = '<style style = "text.css">.colour{background-color:#FF0000}</style>'
 
-# for printing the result with backgroun		
+# for printing the result with background		
 #for dict in sortedList:
 	#print('{price:5s},{address:35s},{location:30s},{brand:15s}'.format(**dict))
 	
